7f1e45afde314e29e856ce1030e88a69ee3abe56
John W. Fail <fail@cenotaph.org>
2014-10-06 01:35:32 +0300

    registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_token, :remember_created_at, :sign_in_count) }
-    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :username, :email,  authentications_attributes: [:id, :provider, :username ], role_ids: [] )}    
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :username,  :email,  authentications_attributes: [:id, :provider, :username ], role_ids: [] )}    
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :name, :username, :password_confirmation, authentications_attributes: [:id, :provider, :username ] ) }
   end
   
