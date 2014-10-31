21a8e41e349718b89441a4168c55689829ef3ed6
John W. Fail <fail@cenotaph.org>
2014-10-31 21:07:15 +0200

    footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   def index
     @places = Place.published
-    @posts = Post.no_project.published.order('published_at DESC').limit(4)
+    @posts = Post.no_project.published.order('published_at DESC').limit(3)
   end
   
   def map_search
