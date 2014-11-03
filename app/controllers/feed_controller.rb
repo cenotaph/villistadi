1c223cb5c5b5d678c4e5a9689218bec203a599c7
John W. Fail <fail@cenotaph.org>
2014-11-03 22:21:31 +0200

    added XML link

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,7 +2,7 @@ class FeedController < ApplicationController
   
   def index
     @feed = Post.published.order(published_at: :desc).limit(25)
-    @feed << Event.limit(25)
+    @feed << Event.approved.limit(25)
     @feed = @feed.flatten.sort_by{|x| x.feed_date }.reverse[0..19]
     respond_to do |format|
       format.any do
